eace3822c05435654cd9f44d458c769b7165d0a2
Cristóbal Martínez <wipeoman@gmail.com>
2021-02-24 17:02:00 +0100

    Initial commit

--- src/core/Routes/Routes.jsx
+++ src/core/Routes/Routes.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import {
-  Switch,
-  Route
+    Switch,
+    Route
 } from "react-router-dom";
-import { AdoptionsPage } from '../../pages/AdoptionsPage/AdoptionsPage';
+import {AdoptionsPage} from '../../pages/AdoptionsPage/AdoptionsPage';
 
-import { ProfilePage } from '../../pages/ProfilePage/ProfilePage';
+import {ProfilePage} from '../../pages/ProfilePage/ProfilePage';
+import {LoginPage} from "../../pages/LoginPage/LoginPage";
 
 export function Routes() {
-    return(
+    return (
         <Switch>
             <Route path="/profile">
-         <ProfilePage></ProfilePage>
-          </Route>
-          <Route path="/adoptions">
-         <AdoptionsPage></AdoptionsPage>
-          </Route>
+                <ProfilePage></ProfilePage>
+            </Route>
+            <Route path="/adoptions">
+                <AdoptionsPage></AdoptionsPage>
+            </Route>
+            <Route path={"/login"}>
+                <LoginPage/>
+            </Route>
             {/* <Route exact path="/">
                 <HomePage />
             </Route> */}
             <Route path="*">
                 <h1>ERROR</h1>
             </Route>
-        </Switch> 
+        </Switch>
     );
 }
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./LoginPage.scss";
+
+export function LoginPage() {
+    return(
+        <div className={"p-login"}>
+            <h1>Login works!</h1>
+        </div>
+    )
+}
