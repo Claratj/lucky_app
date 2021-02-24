af03aee9faa3b7bd2f057d3cb2422f5f08094246
Mercedes Reguera <mmregbri@gmail.com>
2021-02-24 19:16:07 +0100

    adoption details wip

--- src/core/Routes/Routes.jsx
+++ src/core/Routes/Routes.jsx
@@ -18,6 +18,9 @@ export function Routes() {
             <Route path="/profile">
          <ProfilePage></ProfilePage>
           </Route>
+          <Route path="/adoptions/:id/:more">
+         <AdoptionsDeatilPage></AdoptionsDeatilPage>
+          </Route>
           <Route path="/adoptions/:id">
          <AdoptionsDeatilPage></AdoptionsDeatilPage>
           </Route>
