4b93dc71e45a48fc0cc9206a4683a3a57341921f
plex30 <elplanbee@gmail.com>
2021-02-24 12:57:24 +0100

    cambio import routes app.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router
 } from "react-router-dom";
 import './App.css';
-import { Routes } from './core/Routes/Routes';
+import { Routes } from './core/routes/Routes';
 
 function App() {
   return (
