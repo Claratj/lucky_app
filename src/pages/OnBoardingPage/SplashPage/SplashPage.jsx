10509a8c81660db1f4e3621686bdd6c5c242e9a3
Claratj <clara@claratoloba.com>
2021-02-24 18:46:38 +0100

    fotos y carpeta splash onboarding

---
+++ src/pages/OnBoardingPage/SplashPage/SplashPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function SplashPage() {
+    return (
+        <div>
+            <img src="../../assets/img/"></img>
+            <h1>LUCKY</h1>
+        </div>
+    )
+}
