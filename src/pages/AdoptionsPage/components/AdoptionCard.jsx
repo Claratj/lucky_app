40a1590e0fa064912144b4262ac0d2d43c4eda67
Mercedes Reguera <mmregbri@gmail.com>
2021-02-24 14:52:30 +0100

    adoption card wip

--- src/pages/AdoptionsPage/components/AdoptionCard.jsx
+++ src/pages/AdoptionsPage/components/AdoptionCard.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './AdoptionCard.scss';
 
 export function AdoptionCard(props) {
     return(
         <div className="c-adoption-card">
-        <div>
-            <h1>title</h1>
-            {props.process && 
-            <p>process</p>
-            }
-            
+        <Link to={"/adoptions/" + props.id} className="c-adoption-card__link">
+        <div className="c-adoption-card__title">
+            <p className="c-adoption-card__title__name">Adopción de {props.name}</p>
+            {props.status && 
+                <p className="c-adoption-card__title__status">{props.status}</p>
+            }            
         </div>
-        <div>
-            <img src="" alt=""/>
-            <ul>details</ul>
+        <div className="c-adoption-card__details">
+            <img src={props.img} alt="" className="c-adoption-card__details__img"/>
+            <div className="c-adoption-card__details__copy">
+                <p>Nombre</p>
+                <p>Ciudad</p>
+                <p>Sexo</p>
+            </div>
+            <div className="c-adoption-card__details__copy c-adoption-card__details__copy--thin">
+                <p>{props.name}</p>
+                <p>{props.city}</p>
+                <p>{props.gender}</p>
+            </div>
         </div>
-
+        </Link>
         </div>
     );
 }
---
+++ src/pages/AdoptionsPage/pages/AdoptionsDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './AdoptionsDetailPage.scss';
+
+export function AdoptionsDeatilPage() {
+    return(
+        <div className="p-adoptions-deatil">
+            <h1>DETALLE ADOPCION</h1>
+        </div>
+    );
+}
