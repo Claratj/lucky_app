f586a0937329c1414635aa0a93f007fb07e7a5f5
Claratj <clara@claratoloba.com>
2021-02-25 19:07:41 +0100

    estilos splash done

--- src/pages/OnBoardingPage/SplashPage/SplashPage.jsx
+++ src/pages/OnBoardingPage/SplashPage/SplashPage.jsx
@@ -7,8 +7,10 @@ import img from '../../../assets/img/splash.svg';
 export function SplashPage() {
     return (
         <div className="splash-c">
-            <img className="splash-c__img" src={img}></img>
-            <h1 className="splash-c__title">LUCKY</h1>
+            <figure className="fig-c">
+                <img className="fig-c__img" src={img}></img>
+                <h1 className="fig-c__title">LUCKY</h1>
+            </figure>
         </div>
     )
 }
