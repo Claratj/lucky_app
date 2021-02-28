cb6ec3c1eb7ce0de6c4898b24342a487d390d47c
Cristóbal Martínez <wipeoman@gmail.com>
2021-02-28 10:26:00 +0100

    Improved some styling; login choice page pretty much finished

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./LoginPage.scss";
+import {Link} from "react-router-dom";
 
 export function LoginPage() {
     return(
         <div className={"p-login"}>
-            <h2>¿Cómo quieres entrar?</h2>
+            <h3 className={"s-text-style-6"}>¿Cómo quieres entrar?</h3>
+            <Link to={"#"} className={"p-login__button"}><button className={"c-button__dark"}>Usuario</button></Link>
+            <Link to={"#"} className={"p-login__button"}><button className={"c-button__dark"}>Asociación protectora</button></Link>
+            <Link to={"#"} className={"p-login__text"}><p className={"s-detail-1"}>Registrarse en otro momento</p></Link>
         </div>
     )
 }
