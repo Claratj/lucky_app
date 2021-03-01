8fab79f4310e8579a3a15cc26cf81860b712d1ba
Henrique Lijo <henriquelijo@gmail.com>
2021-03-01 15:37:05 +0100

    changes

--- src/pages/HomePage/HomeCard/HomeCard.jsx
+++ src/pages/HomePage/HomeCard/HomeCard.jsx
@@ -7,49 +7,58 @@ import confi from '../../../assets/img/imgHomeCard/confi.png';
 import eventos from '../../../assets/img/imgHomeCard/eventos.png';
 import protectora from '../../../assets/img/imgHomeCard/protectora.png';
 import salir from '../../../assets/img/imgHomeCard/salir.png';
+import Footer from '../../../core/Footer/Footer';
 
 export function HomeCard() {
 
     
 
     return (
-        <div>
-            <div>
+        <div className="container">
+            <div className="container-img">
+                
+                <img src={protectora}></img>
                 <p>
                     Asociaciones protectoras
                 </p>
-                <img src={protectora}></img>
             </div>
-            <div>
+            <div className="container-img">
+                
+                <img src={eventos}></img>
                 <p>
                     Eventos
                 </p>
-                <img src={eventos}></img>
             </div>
-            <div>
+            <div className="container-img">
+                
+                <img src={blogCopy}></img>
                 <p>
                     Curiosidades
                 </p>
-                <img src={blogCopy}></img>
             </div>
-            <div>
+            <div className="container-img">
+                
+                <img src={ayuda}></img>
                 <p>
                     Ayuda
                 </p>
-                <img src={ayuda}></img>
             </div>
-            <div>
+            <div className="container-img">
+                
+                <img src={confi}></img>
                 <p>
                     Configuración 
                 </p>
-                <img src={confi}></img>
             </div>
-            <div>
+            <div className="container-img session">
+                
+                <img src={salir}></img>
                 <p>
                     Cerrar sesión
                 </p>
-                <img src={salir}></img>
             </div>
+
+            <Footer />
 
         </div>
         
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,33 +7,35 @@ import perroMotero from '../../assets/img/imgHomePage/perroMotero.png';
 import Footer from '../../core/Footer/Footer';
 
 
+
 export function HomePage() {
     return (
-        <main>
+        <section className="container">
 
-            <div className="container-h3">
-              <h3>Hola Celia!</h3>
+            <div className="container-welcome">
+              <h3 className="container-welcome__h3">¡Hola Celia!</h3>
             </div>
             {/* <BootstrapCarousel /> */}
-            <div className="container-img">
-                <figure>
-                    <img src={chinchillas}/>
-                    <p>10 curiosidades sobre las chichillas.</p>    
+        <div className="container-img">
+          <p className="container-img__title">Novedades</p>
+                <figure className="container-img__figure">
+                    <img className="container-img__img" src={chinchillas}/>
+                    <p className="container-img__p">10 curiosidades sobre las chichillas.</p>    
                 </figure>
 
-                <figure>
-                    <img src={iguana} />
-                  <p>¿Sabes qué comen las iguanas?</p>  
+                <figure className="container-img__figure">
+                    <img className="container-img__img" src={iguana} />
+                  <p className="container-img__p">¿Sabes qué comen las iguanas?</p>  
                 </figure>
 
-                <figure>
-                    <img src={perroMotero} />
-                  <p>10 lugares para visitar con tu perro en Madrid.</p>  
+                <figure className="container-img__figure">
+                    <img className="container-img__img" src={perroMotero} />
+                  <p className="container-img__p">10 lugares para visitar con tu perro en Madrid.</p>  
                 </figure>
 
             </div>
             <Footer />
-        </main>
+        </section>
     )
 }
 export default HomePage;
