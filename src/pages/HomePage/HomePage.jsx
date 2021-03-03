28e7cbefd76af8b962a048acf0b0cdc4651662c3
Mercedes Reguera <mmregbri@gmail.com>
2021-03-03 12:27:54 +0100

    probando llamar a la api. error con CORS policy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../core/Footer/Footer';
 import './HomePage.scss';
 //import BootstrapCarousel from './Carousel/BootstrapCarousel';
@@ -6,12 +6,39 @@ import chinchillas from '../../assets/img/imgHomePage/chinchillas.png';
 import iguana from '../../assets/img/imgHomePage/iguana.png';
 import perroMotero from '../../assets/img/imgHomePage/perroMotero.png';
 import Footer from '../../core/Footer/Footer';
+import { API } from '../../shared/consts/api.consts';
 
 export function HomePage() {
+  const [news, setNews] = useState([]);
+
+//   const getNews = fetch('http://localhost:5300/news', {
+//     method: 'GET',
+//     headers: {
+//         'Accept': 'application/json',
+//         'Content-Type': 'application/json',
+//         'Access-Control-Allow-Origin': '*',
+//     },
+//     credentials: 'include',
+// }).then((results)=> {
+//   setNews(results.results);
+//   console.log(results);
+//   });
+  
+  const getNews = () =>{
+    console.log('hola');
+    API.get('/news').then((results)=> {
+    setNews(results.results);
+    console.log(results);
+    });
+  }
+
+  useEffect(getNews, []);
+  console.log(news);
+
     return (
         <div className="container flex flex-column align-items-center">
           <div className="welcome">
-            <h3 className="welcome__title s-text-style-5">¡Hola Celia!</h3>
+            <h3 className="welcome__title s-text-style-5">¡Hola Celia!</h3> 
           </div>
             {/* <BootstrapCarousel /> */}
           <div>
