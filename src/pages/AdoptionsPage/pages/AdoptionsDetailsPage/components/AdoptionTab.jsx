1a76e4f8ed564bddb9d7a6babdf8519c9f80f4de
Mercedes Reguera <mmregbri@gmail.com>
2021-03-07 16:01:47 +0100

    change

--- src/pages/AdoptionsPage/pages/AdoptionsDetailsPage/components/AdoptionTab.jsx
+++ src/pages/AdoptionsPage/pages/AdoptionsDetailsPage/components/AdoptionTab.jsx
@@ -6,16 +6,21 @@ import './AdoptionTab.scss';
 
 export function AdoptionTab(props){
 
-    const [selectedDate, setSelectedDate] = useState(new Date());
+    // const [selectedDate, setSelectedDate] = useState(new Date());
 
-    const getDate = () => {
-        setSelectedDate(Date.now());
-        console.log(selectedDate);
-    }
+    // const getDate = () => {
+    //     setSelectedDate(Date.now());
+    //     console.log(selectedDate);
+    // }
 
-    useEffect(getDate, []);
+    // useEffect(getDate, []);
+
+    const today = new Date();
+    console.log(today);
+    // today.now();
 
     return(
+
         <div className="container flex flex-column align-items-center tab">
         <div className="tab__address">
             <p className="s-body-2 tab__copy">Dirección</p>
@@ -23,14 +28,24 @@ export function AdoptionTab(props){
             MAP
             <form noValidate className="appointment-form">
                 <label className="s-body-2 tab__copy tab__copy--top">Día</label>
-                <TextField
+                {/* {today&& <TextField
                     id="date"
+                    // label="Birthday"
                     type="date"
-                    defaultValue={selectedDate}
+                    defaultValue="27-03-2020"
                     InputLabelProps={{
                         shrink: true,
                     }}
-                />
+                />} */}
+                 <TextField
+    id="date"
+    type="date"
+    defaultValue={new Date()}
+    // className={classes.textField}
+    InputLabelProps={{
+      shrink: true,
+    }}
+  />
                 <label className="s-body-2 tab__copy tab__copy--top">Hora</label>
                 <TextField id="hola" label="¿A qué hora puedes venir?" defaultValue="9.00h" select>
                     <MenuItem type="time" value="9">9.00h</MenuItem>
