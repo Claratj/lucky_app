1bea186c250740297ed1399efb2d21f3c276f597
Mercedes Reguera <mmregbri@gmail.com>
2021-03-08 19:46:13 +0100

    trabjando en popup filter adoptions

--- src/shared/SearchBar/SearchBar.jsx
+++ src/shared/SearchBar/SearchBar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import {NavLink} from 'react-router-dom';
 import iconFilter from '../../assets/img/filtros.png';
 import iconBack from '../../assets/img/atras.png';
 import iconSearch from '../../assets/img/buscar.png';
 import './SearchBar.scss';
+import { PopUpContext } from '../Context/PopUpContext';
 
 export default function SearchBar(props) {
+    const {pop, setPop} = useContext(PopUpContext);
+    console.log(pop);
+
     return (
         <div className="c-searchBar">
            <div className="c-searchBar__back"></div>
@@ -17,8 +21,7 @@ export default function SearchBar(props) {
                    props.handleChange(input)}}><img src={iconSearch} alt=""/></button>
            </div> 
            <div className="c-searchBar__filter">
-               <NavLink to="/"><img src={iconFilter} className="c-search__imgFilter" alt=""/></NavLink>
-           </div> 
+            <img src={iconFilter} className="c-search__imgFilter" alt="" onClick={props.click}/></div>
         </div>
     )
 }
