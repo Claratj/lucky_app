df8d3baf3b5cd1cabe76b8fd2df188a2ed09e12d
Mercedes Reguera <mmregbri@gmail.com>
2021-03-09 13:09:17 +0100

    funcionalidad filtro adopcion

--- src/pages/AdoptionsPage/components/PopUpFilter/PopUpFilter.jsx
+++ src/pages/AdoptionsPage/components/PopUpFilter/PopUpFilter.jsx
@@ -1,17 +1,49 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { PopUpContext } from '../../../../shared/Context/PopUpContext';
 import PopUp from '../../../../shared/PopUp/PopUp';
+
+import '../../../PetsPage/components/FilterPets/FilterPets.scss';
 
 export function PopUpFilter(props) {
     const {pop, setPop} = useContext(PopUpContext);
 
-    // const closePopUp = () => {
-    //     setPop(false);
-    // }
+    const clearFilter = () => {
+        const inputs = document.querySelectorAll('input')
+        inputs.forEach(input => {
+            input.disabled = false;
+            input.checked = false;
+        });
+    }
+
     return(
         <PopUp show={pop} onClose={props.onClose}>
         <div>
-        hola
+            Filtros
+            <input id='accepted' name='accepted' className="c-input-img__input" value='accepted' onClick={props.filter} 
+            // onClick={inputSelected} 
+            type="checkbox" alt="Submit" />
+                        <label htmlFor='accepted' className="c-input-img__label" >
+                            {/* <span className="icon-dog size" />  */}
+                            Completado
+                        </label>
+
+            <input id='process' name='process' className="c-input-img__input" value='process' onClick={props.filter} 
+            // onClick={inputSelected} 
+            type="checkbox" alt="Submit" />
+                        <label htmlFor='process' className="c-input-img__label" >
+                            {/* <span className="icon-dog size" />  */}
+                            En proceso
+                        </label>
+
+            <input id='rejected' name='rejected' className="c-input-img__input" value='rejected' onClick={props.filter} 
+            // onClick={inputSelected} 
+            type="checkbox" alt="Submit" />
+                        <label htmlFor='rejected' className="c-input-img__label" >
+                            {/* <span className="icon-dog size" />  */}
+                            Rechazado
+                        </label>
+                        <button type="reset" onClick={clearFilter}>Borrar filtros</button>
+                        <button type="submit" onClick={props.submit}>Enviar filtros</button>
         </div>
         </PopUp>
     );
